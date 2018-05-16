1e3c6ab1f71f4b8c592d76b937ab589973b22d78
quepas <kiepas@agh.edu.pl>
2018-05-16 15:26:48 +0200

    More scripts

--- influence.commentTimeline.R
+++ influence.commentTimeline.R
@@ -3,9 +3,11 @@ library(dplyr)
 # Calculate min, avg, median, max time to comment in minutes
 influence.commentTimeline <- function(user_posts) {
   user_posts %>%
-    group_by(post_id) %>%
-    summarise(min = min(action_timestamp - post_timestamp) / 1000 / 60,
-              avg = mean(action_timestamp - post_timestamp) / 1000 / 60,
-              median = median(action_timestamp - post_timestamp) / 1000 / 60,
-              max = max(action_timestamp - post_timestamp) / 1000 / 60)
+    group_by(user_id, post_id) %>%
+    summarise(minComment = min(action_timestamp - post_timestamp) / 1000 / 60,
+              avgComment = mean(action_timestamp - post_timestamp) / 1000 / 60,
+              medianComment = median(action_timestamp - post_timestamp) / 1000 / 60,
+              maxComment = max(action_timestamp - post_timestamp) / 1000 / 60) %>%
+    group_by(user_id) %>%
+    summarise(medianUserComment = median(medianComment), avgUserComment = mean(medianComment))
 }
--- testTimeModel.R
+++ testTimeModel.R
@@ -1,5 +1,6 @@
 source("influence.plotEfficiency.R")
 source("influence.plotEfficiency2.R")
+source("influence.plotEfficiency3.R")
 source("influence.commentTimeline.R")
 
 ## Some posts and their properties (time window 7d)
@@ -8,14 +9,14 @@ source("influence.commentTimeline.R")
 # 3) 1540784020687880833  -- more posts, but all the rest going down
 # 4) -3000490335411638731 -- balancing!
 # 5) -5562342665076700406 -- all go up!
-user_posts <- top100posts %>% filter(user_id == "1540784020687880833")#sample(top100posts$user_id, 1))
+user_posts <- top100posts %>% filter(user_id == sample(top100posts$user_id, 1))
 
 # Window settings
 a_day_in_ms <- 1000 * 3600 * 24
 window <- list(
   start=min(top100posts$post_timestamp), # from when we apply the windows
   end=max(top100posts$post_timestamp),   # until when we apply the windows
-  size=4 * a_day_in_ms)                  # the size of the applied windows
+  size=7 * a_day_in_ms)                  # the size of the applied windows
 
 influence.plotEfficiency(user_posts, window)
 #timeline <- influence.commentTimeline(user_posts)
@@ -23,8 +24,17 @@ influence.plotEfficiency(user_posts, window)
 window <- list(
   start=min(top100posts$post_timestamp), # from when we apply the windows
   end=max(top100posts$post_timestamp),   # until when we apply the windows
-  size=4 * a_day_in_ms,                  # the size of the applied windows
+  size=7 * a_day_in_ms,                  # the size of the applied windows
   step=1 * a_day_in_ms)                  # moving window step
 
 ## TODO: make PRI absolute from size of the time window?
-influence.plotEfficiency2(user_posts, window)
+#influence.plotEfficiency2(user_posts, window)
+
+just_7_days <- user_posts %>% filter(post_timestamp <= window$start + window$size * 2 & action_timestamp <= window$start + window$size * 2)
+
+influence.plotEfficiency3(just_7_days)
+
+timeline <- influence.commentTimeline(top100posts)
+par(mfrow=c(1, 2))
+plot(density(timeline$medianUserComment))
+plot(density(timeline$avgUserComment))
