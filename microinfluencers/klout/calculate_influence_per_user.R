cd2079490ff974f564171f3778e21548fa2f8023
quepas <kiepas@agh.edu.pl>
2018-07-18 11:20:26 +0200

    Adding influence calculation script

--- microinfluencers/klout/calculate_influence_per_user.R
+++ microinfluencers/klout/calculate_influence_per_user.R
@@ -1,10 +1,38 @@
 library(dplyr)
 
-engagement <- read.csv("engagement.csv", header = T, stringsAsFactors = T, colClasses = c("factor", rep("numeric", 2)))
-# FC doesn't contain the num of posts ! Is it true? They are the same in both CSVs
-fc <- read.csv("fc_A.csv", header = T, stringsAsFactors = T, colClasses = c("factor", "numeric"))
+engagement <- read.csv("engagement_spread.csv",
+                       header = T,
+                       stringsAsFactors = T,
+                       colClasses = c("factor", rep("numeric", 2)))
+fc <- read.csv("fc_A.csv",
+               header = T,
+               stringsAsFactors = T,
+               colClasses = c("factor", "numeric"))
 
-engagement <- engagement %>%
-  mutate(influence = (engag_numerator / engag_denominator) * exp(1/engag_denominator)) %>%
-  arrange(influence) %>%
-  filter(engag_denominator > 100)
+# Compute 
+computed_influence <- engagement %>%
+  mutate(influence = engagement_value * exp(1/fc$engag_denominator)) %>%
+  filter(spread > 100) %>%
+  arrange(desc(influence))
+
+# Save users' influence ordered by the influence
+write.csv(computed_influence %>% select(user_id, spread, influence),
+          "result_by_influence.csv",
+          row.names = F,
+          quote = F)
+# Save users' influence ordered by user's audience (spread)
+write.csv(computed_influence %>% select(user_id, spread, influence) %>% arrange(desc(spread)),
+          "result_by_spread.csv",
+          row.names = F,
+          quote = F)
+
+# Compute all
+computed_influence <- engagement %>%
+  mutate(influence = engagement_value * exp(1/fc$engag_denominator)) %>%
+  arrange(desc(influence))
+
+# Save users' influence ordered by user's audience (spread) for all
+write.csv(computed_influence %>% select(user_id, spread, influence) %>% arrange(desc(spread)),
+          "result_by_spread_all.csv",
+          row.names = F,
+          quote = F)
