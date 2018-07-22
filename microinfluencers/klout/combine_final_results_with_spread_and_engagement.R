35901b96bd36ec11bddcee844400531bdec9682b
quepas <kiepas@agh.edu.pl>
2018-07-21 23:34:51 -0400

    Updated results

--- microinfluencers/klout/combine_final_results_with_spread_and_engagement.R
+++ microinfluencers/klout/combine_final_results_with_spread_and_engagement.R
@@ -13,5 +13,14 @@ pr_order$user_id <-format(pr_order$user_id, scientific = FALSE)
 arim_order <- merge(arim_order, spread_engagement, by = "user_id")
 pr_order <- merge(spread_engagement, pr_order, by = "user_id")
 
+# format data
+arim_order$pr_normalized <- round(arim_order$pr_normalized, digits = 6)
+arim_order$influence_normalised <- round(arim_order$influence_normalised, digits = 6)
+arim_order$engagement_value <- round(arim_order$engagement_value, digits = 6)
+
+pr_order$pr_normalized <- round(pr_order$pr_normalized, digits = 6)
+pr_order$influence_normalised <- round(pr_order$influence_normalised, digits = 6)
+pr_order$engagement_value <- round(pr_order$engagement_value, digits = 6)
+
 write.csv(arim_order, "trimming_microinfluencers/results_final/microinfluencers_100-500_arim_order_full.csv", row.names = F)
 write.csv(pr_order, "trimming_microinfluencers/results_final/microinfluencers_100-500_pr_order_full.csv", row.names = F)
--- microinfluencers/pinterest/combine_final_results_with_spread_and_engagement.R
+++ microinfluencers/pinterest/combine_final_results_with_spread_and_engagement.R
@@ -17,5 +17,14 @@ pr_order$user_id <-format(pr_order$user_id, scientific = FALSE)
 arim_order <- merge(arim_order, spread_engagement, by = "user_id")
 pr_order <- merge(spread_engagement, pr_order, by = "user_id")
 
+# format data
+arim_order$pr_normalized <- round(arim_order$pr_normalized, digits = 6)
+arim_order$influence_normalised <- round(arim_order$influence_normalised, digits = 6)
+arim_order$engagement <- round(arim_order$engagement, digits = 6)
+
+pr_order$pr_normalized <- round(pr_order$pr_normalized, digits = 6)
+pr_order$influence_normalised <- round(pr_order$influence_normalised, digits = 6)
+pr_order$engagement <- round(pr_order$engagement, digits = 6)
+
 write.csv(arim_order, "trimming_microinfluencers/results_final/microinfluencers_100-500_arim_order_full.csv", row.names = F)
 write.csv(pr_order, "trimming_microinfluencers/results_final/microinfluencers_100-500_pr_order_full.csv", row.names = F)
