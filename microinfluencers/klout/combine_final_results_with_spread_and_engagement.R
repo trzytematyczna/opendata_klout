9296f0170677b54ccfd187e40c237f50a742409c
quepas <kiepas@agh.edu.pl>
2018-07-21 22:11:32 -0400

    Adding combined results

---
+++ microinfluencers/klout/combine_final_results_with_spread_and_engagement.R
@@ -0,0 +1,17 @@
+spread_engagement <- read.csv("arim/engagement_spread.csv")
+
+arim_order <- read.csv("trimming_microinfluencers/results_final/microinfluencers_100-500_arim_order.csv")
+pr_order <- read.csv("trimming_microinfluencers/results_final/microinfluencers_100-500_pr_order.csv")
+
+colnames(arim_order) <- gsub("joined.", "", colnames(arim_order))
+colnames(pr_order) <- gsub("joined.", "", colnames(pr_order))
+
+spread_engagement$user_id <-format(spread_engagement$user_id, scientific = FALSE)
+arim_order$user_id <-format(arim_order$user_id, scientific = FALSE)
+pr_order$user_id <-format(pr_order$user_id, scientific = FALSE) 
+
+arim_order <- merge(arim_order, spread_engagement, by = "user_id")
+pr_order <- merge(spread_engagement, pr_order, by = "user_id")
+
+write.csv(arim_order, "trimming_microinfluencers/results_final/microinfluencers_100-500_arim_order_full.csv")
+write.csv(pr_order, "trimming_microinfluencers/results_final/microinfluencers_100-500_pr_order_full.csv")
